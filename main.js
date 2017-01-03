29e3465668b6a8a77003d5cd268fa5c24b6736c0
mspavanelli <mspavanelli@gmail.com>
2017-01-03 21:35:47 -0200

    Versão Nova (2017)
    Falta ainda a sinopse da parte de inglês

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-var scroll = function() {
-	$('.areas figure').click(function() {
-		$('html, body').animate({
-			scrollTop: $('#detalhes').offset().top - 30
-		});
-	});
-}
-
 $(document).ready(function() {
-	
-	// scroll();
 
-	$('.areas img').click(function() {
-		$(this).addClass('animated jello').removeClass('animated jello');
-	});
+	var scroll = function(elemento, alvo) {
+		$(elemento).click(function() {
+			$(alvo).animatescroll({padding:51});
+		});
+	}
+
+	scroll(".navbar-brand", "html");
+
+	var areas = ['ingles', 'financas', 'redacao', 'hipnose', 'coaching'];
+	areas.forEach(item => scroll(".item-"+item, ".area-"+item));
 
 });
-
-
-// $("#button").click(function() {
-//   $("#button").addClass("fadeInDown animated").one('animationend webkitAnimationEnd oAnimationEnd', function() {
-//     $("#button").removeClass();
-//   });
-// });
